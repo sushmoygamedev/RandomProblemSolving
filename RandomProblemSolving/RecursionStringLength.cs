fbdfd227494469c1f8bf883bc39319b249d8aeb1
sushmoygamedev <sushmoygamedev@gmail.com>
2021-07-05 00:21:00 +0600

    Recursion String Length Re-upload

---
+++ RandomProblemSolving/RecursionStringLength.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RandomProblemSolving
+{
+    class recursion1
+    {
+        /*public static int CalculateSumRec(int n, int m)
+        {
+            int sum = n;
+
+            if (n < m)
+            {
+                n++;
+                return sum += CalculateSumRec(n, m);
+            }
+
+            return sum;
+        }*/
+
+        public static int recStringLen(string str)
+        {
+            if (str == "")
+            {
+                return 0;
+            }
+            else
+            {
+                return recStringLen(str.Substring(1)) + 1;
+            }
+        }
+    }
+}
