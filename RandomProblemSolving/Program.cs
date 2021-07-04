cfbd004704cc2db005d60537050341164659b124
sushmoygamedev <sushmoygamedev@gmail.com>
2021-07-05 00:11:03 +0600

    Updating files

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -5,7 +5,17 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            
+            string str = "benefit";
+
+            /*Console.WriteLine(str.Substring(1, str.Length - 2));*/
+            /*for (int i = 1; i < str.Length-1; i++)
+            {
+                Console.Write(str[i]-1);
+            }*/
+            for (int i = 1; i < str.Length; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
         }
     }
 }
