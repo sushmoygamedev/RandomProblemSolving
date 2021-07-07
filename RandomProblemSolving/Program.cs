6f0f90076d41426c41936e2c88aa4f4e436cd4b4
sushmoygamedev <sushmoygamedev@gmail.com>
2021-07-08 02:39:29 +0600

    Updated

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -1,21 +1,12 @@
 ﻿using System;
+
 namespace RandomProblemSolving
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string str = "benefit";
-
-            /*Console.WriteLine(str.Substring(1, str.Length - 2));*/
-            /*for (int i = 1; i < str.Length-1; i++)
-            {
-                Console.Write(str[i]-1);
-            }*/
-            for (int i = 1; i < str.Length; i++)
-            {
-                Console.WriteLine(str[i]);
-            }
+            
         }
     }
 }
