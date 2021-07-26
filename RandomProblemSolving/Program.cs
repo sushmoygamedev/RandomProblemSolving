d20fe8aa5dd4061a383ce59423a1bdd0866ee6ae
sushmoygamedev <sushmoygamedev@gmail.com>
2021-07-27 00:43:58 +0600

    UPDATE - JULY-26-2021-00:43

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -6,24 +6,7 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            string[] strList = new string[] { "John", "Taylor", "John", "Pikachu", "Marco", "Macino", "Pikachu", "Pikachu", "Machino", "Macino" };
-            int strLen = strList.Length;
-            Array.Sort(strList);
-            int j = 0;
-            for (int i = 0; i < strLen - 1; i++)
-            {
-                if (strList[i] != strList[i+1])
-                {
-                    strList[j] = strList[i];
-                    j++;
-                }
-            }
-            strList[j++] = strList[strLen - 1];
-
-            for (int i = 0; i < j; i++)
-            {
-                Console.WriteLine(strList[i]);
-            }
+            
         }
     }
 }
