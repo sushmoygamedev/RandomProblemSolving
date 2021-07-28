f771d873749cd73e7a50c68827a96f2e30cb5359
sushmoygamedev <sushmoygamedev@gmail.com>
2021-07-28 17:27:44 +0600

    JULY 28 2021 17:27

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -7,17 +7,27 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            int[] numList = new int[] { 1, 0, 1, 0 };
+            /*int[] numList = new int[] { 1, 0, 1, 0 };
             int[] removeDuplicate = numList.Distinct().ToArray();
             int[] newNumList = new int[removeDuplicate.Length];
             string result = string.Empty;
-            
+
             for (int i = 0; i < removeDuplicate.Length; i++)
             {
                 newNumList[i] = removeDuplicate[i];
-                result += numList[i] + ", ";
+                Console.WriteLine(numList[i]);
+            }*/
+
+
+            string str = "hello";
+            char[] c = str.ToCharArray();
+            Console.WriteLine(string.Join('-', c));
+            string newStr = string.Empty;
+            string anotherSTr = string.Empty;
+            for (int i = 0; i < str.Length; i++)
+            {
+                newStr += str[i] + "-";
             }
-            Console.WriteLine(result);
         }
     }
 }
