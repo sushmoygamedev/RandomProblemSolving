368587d9ceb5c582a1f3954e2f89b556c3a709e9
sushmoygamedev <sushmoygamedev@gmail.com>
2021-07-31 23:34:53 +0600

    JULY 31 2021 23:32

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -1,33 +1,32 @@
 ﻿using System;
 using System.Linq;
-
 namespace RandomProblemSolving
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*int[] numList = new int[] { 1, 0, 1, 0 };
-            int[] removeDuplicate = numList.Distinct().ToArray();
-            int[] newNumList = new int[removeDuplicate.Length];
+            //USING DISTINC METHOND FROM LINQ LIBRARY
+            //FIRST INT ARRAY
+            int[] numList = { 1, 2, 2, 2, 3, 2, 5, 2, 6, 6, 3, 7, 1, 2, 5 };
+            int[] numListRemoveDuplicate = numList.Distinct().ToArray();
+            int[] newNumList = new int[numListRemoveDuplicate.Length];
             string result = string.Empty;
-
-            for (int i = 0; i < removeDuplicate.Length; i++)
+            for (int i = 0; i < numListRemoveDuplicate.Length; i++)
             {
-                newNumList[i] = removeDuplicate[i];
-                Console.WriteLine(numList[i]);
-            }*/
-
-
-            string str = "hello";
-            char[] c = str.ToCharArray();
-            Console.WriteLine(string.Join('-', c));
-            string newStr = string.Empty;
-            string anotherSTr = string.Empty;
-            for (int i = 0; i < str.Length; i++)
-            {
-                newStr += str[i] + "-";
+                //numList[i] = numListRemoveDuplicate[i];
+                newNumList[i] = numListRemoveDuplicate[i];
+                /*Console.WriteLine(newNumList[i]);*/
+                if (newNumList[i] == newNumList[newNumList.Length - 1])
+                {
+                    result += newNumList[i];
+                }
+                else
+                {
+                    result += newNumList[i] + ",";
+                }
             }
+            Console.WriteLine(result);
         }
     }
 }
