0a61f1329ec783253219ab90bcbf13c2e86df7e4
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-15 23:44:57 +0600

    Change Log: AUG 15 2021 23:44

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -6,7 +6,25 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            
+            // SOLVE #1
+            /*string str = "732 32";
+            string zipCode = "";
+            bool isValid = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    zipCode += str[i];
+                }
+            }
+            Console.WriteLine("zipCode: " + zipCode + "\n" + "zipCode Lenght: " + zipCode.Length);
+            if (zipCode.Length == 5)
+            {
+                isValid = true;
+            }
+            Console.WriteLine("Is the ZIP code valid: " + isValid);*/
+
+            // SOLVE #2
         }
     }
 }
