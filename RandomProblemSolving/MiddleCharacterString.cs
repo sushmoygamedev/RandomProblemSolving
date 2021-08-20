7735edb8e8951481a8ddfa33606498cfc23cdb69
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-21 01:51:04 +0600

    Change Log: 1 New Solution AUG 21 2021 01:50

---
+++ RandomProblemSolving/MiddleCharacterString.cs
@@ -0,0 +1,30 @@
+﻿// TRIGGER
+/*string str = "testing";
+Console.WriteLine(MiddleCharacterString.MiddleChar(str));*/
+using System;
+
+namespace RandomProblemSolving
+{
+    public class MiddleCharacterString
+    {
+        public static string MiddleChar(string str)
+        {
+            int strLen = str.Length;
+            string middleChar = string.Empty;
+
+            Console.WriteLine(str.Length / 2);
+            Console.WriteLine(strLen % 2);
+
+            if (strLen % 2 != 0)
+            {
+                middleChar += (str[strLen / 2]);
+            }
+            else
+            {
+                middleChar += (str[strLen / 2 - 1] + "" + str[strLen / 2]);
+            }
+
+            return middleChar;
+        }
+    }
+}
