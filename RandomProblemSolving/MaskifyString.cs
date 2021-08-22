f886a68b771b53bbcf238554f4185096e172547d
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-22 14:56:17 +0600

    Change Log: New Solution AUG 22 2021 14:56

---
+++ RandomProblemSolving/MaskifyString.cs
@@ -0,0 +1,36 @@
+﻿// TRIGGER
+/*string str = "asfssdf12313jlsfj";
+MaskifyString.Makify(str);*/
+using System;
+
+namespace RandomProblemSolving
+{
+    public class MaskifyString
+    {
+        public static void Makify(string str)
+        {
+            string maskify = string.Empty;
+
+            for (int i = 0; i < str.Length - 4; i++)
+            {
+                maskify += "#";
+            }
+
+            if (str.Length > 4)
+            {
+                Console.WriteLine(maskify + "" + str.Substring(str.Length - 4));
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine("\"\"");
+                }
+                else
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
