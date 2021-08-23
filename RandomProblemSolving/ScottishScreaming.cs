0e3f032ca113c6b8ff4ead04d1123e83b644c47d
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-23 23:29:49 +0600

    Change Log: 2 New Solution AUG 23 2021 23:29

---
+++ RandomProblemSolving/ScottishScreaming.cs
@@ -0,0 +1,30 @@
+﻿// TRIGGER
+/*string str = "Mr. Fox was very naughty".ToLower();
+Console.WriteLine(ScottishScreaming.Screaming(str));*/
+
+namespace RandomProblemSolving
+{
+    public class ScottishScreaming
+    {
+        public static string Screaming(string str)
+        {
+            string resultStr = string.Empty;
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case 'a':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        resultStr += 'e';
+                        break;
+                    default:
+                        resultStr += str[i];
+                        break;
+                }
+            }
+            return (resultStr.ToUpper());
+        }
+    }
+}
---
+++ RandomProblemSolving/StrangePair.cs
@@ -0,0 +1,54 @@
+﻿// TRIGGER
+/*string str1 = "";
+string str2 = "";
+
+StrangePair instence = new StrangePair();
+instence.str1 = str1;
+instence.str2 = str2;
+Console.WriteLine(instence.CheckStrangePair());*/
+
+using System;
+
+namespace RandomProblemSolving
+{
+    public class StrangePair
+    {
+        public string str1;
+        public string str2;
+        public string  CheckStrangePair()
+        {
+            bool isPair = false;
+
+            /*if (string.IsNullOrEmpty(str1) || string.IsNullOrEmpty(str2))
+            {
+                isPair = true;
+            }
+            else
+            {
+                if (str1.StartsWith(str2[str2.Length - 1]) && str1.EndsWith(str2[0]))
+                {
+                    isPair = true;
+                }
+            }
+
+            Console.WriteLine(isPair);*/
+
+            if (string.IsNullOrEmpty(str1) || string.IsNullOrEmpty(str2))
+            {
+                Console.WriteLine("Empty or Just Spaces");
+
+                isPair = true;
+            }
+            else
+            {
+                Console.WriteLine(str1[0] + " == " + str2[str2.Length - 1] + "\n" + str1[str1.Length - 1] + " == " + str2[0]);
+                if (str1[0] == str2[str2.Length - 1] && str1[str1.Length - 1] == str2[0])
+                {
+                    isPair = true;
+                }
+            }
+            return ("isPair: " + isPair);
+
+        }
+    }
+}
