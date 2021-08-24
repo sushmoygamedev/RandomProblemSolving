c0c54685391f935e598f6bc45abf78e3d8144320
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-24 23:07:24 +0600

    Change Log: Test Code AUG 24 2021 23:07

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -6,7 +6,29 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            
+            /*string str = "%fd76$fd(-)6GvKlO.";
+            string newStr = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetterOrDigit(str[i]) || str[i] == ' ' || str[i] == '-' || str[i] == '_')
+                {
+                    newStr += str[i];
+                }
+            }
+            Console.WriteLine(newStr);*/
+
+            string str = "-123) 456-7890";
+            bool isValid = false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(' && str[i] == ')' || str[i] == '-')
+                {
+                    isValid = true;
+                }
+            }
+            Console.WriteLine(isValid);
         }
     }
 }
