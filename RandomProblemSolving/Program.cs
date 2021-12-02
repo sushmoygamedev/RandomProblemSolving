401f7d9cb1d8fe13cc073b660a9aabe9222bf7d0
sushmoygamedev <86609630+sushmoygamedev@users.noreply.github.com>
2021-12-03 02:10:00 +0600

    Change Log: IDE Update 3 DEC 2021

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -6,7 +6,17 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            
+            string str = "abcd";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(str[0]);
+                for (int j = 0; j < i; j++)
+                {
+                    Console.WriteLine(str[i]);
+                }
+            }
+
         }
     }
 }
