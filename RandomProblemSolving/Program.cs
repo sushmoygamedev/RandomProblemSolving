2295e1f2cab3d29a2e9e15edd785bf2ef3295143
sushmoygamedev <86609630+sushmoygamedev@users.noreply.github.com>
2022-03-24 03:14:00 +0600

    Change Log: Black Slate

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -9,52 +9,7 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            #region Alterning Caps
-            /*string str = "How are you?";
-            char[] strChar = str.ToCharArray();
-            int charCounter = 0;
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (str[i] == ' ')
-                {
-                    continue;
-                }
-                if (charCounter % 2 == 0)
-                {
-                    strChar[i] = char.ToUpper(strChar[i]);
-                }
-                else
-                {
-                    strChar[i] = char.ToLower(strChar[i]);
-                }
-                charCounter++;
-                Console.WriteLine(charCounter);
-            }
-            string newStr = new string(strChar);
-            Console.WriteLine(newStr);*/
-            #endregion
-
-            string str = "OMG this website is awesome!";
-            char[] strChar = str.ToCharArray();
-            int j = 0;
-            for (int i = 0; i < strChar.Length; i++)
-            {
-                if (strChar[i] == ' ')
-                {
-                    continue;
-                }
-                if (j % 2 == 0)
-                {
-                    strChar[i] = Char.ToUpper(strChar[i]);
-                }
-                else
-                {
-                    strChar[i] = Char.ToLower(strChar[i]);
-                }
-                j++;
-            }
-            string newStr = new string(strChar);
-            Console.WriteLine(newStr);
+            
         }
     }
 }
