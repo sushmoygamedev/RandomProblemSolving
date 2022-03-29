d171195efe9d67df28c89eb2eaf580254f339e65
sushmoygamedev <sushmoygamedev@gmail.com>
2022-03-30 00:00:21 +0600

    Change Log; Fixing Issues MAR-30-2022 00:00

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -9,7 +9,31 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            
+            #region Alterning Caps
+            /*string str = "OMG this website is awesome!";
+            char[] strChar = str.ToCharArray();
+            int j = 0;
+            for (int i = 0; i < strChar.Length; i++)
+            {
+                if (strChar[i] == ' ')
+                {
+                    continue;
+                }
+                if (j % 2 == 0)
+                {
+                    strChar[i] = Char.ToUpper(strChar[i]);
+                }
+                else
+                {
+                    strChar[i] = Char.ToLower(strChar[i]);
+                }
+                j++;
+            }
+            string newStr = new string(strChar);
+            Console.WriteLine(newStr);*/
+            #endregion
+
+
         }
     }
 }
