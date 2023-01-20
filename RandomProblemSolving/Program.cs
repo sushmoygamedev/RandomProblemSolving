445c8edda66dca3f84e7b830532208fa8f72fefd
sushmoygamedev <sushmoygamedev@gmail.com>
2023-01-21 01:11:40 +0600

    terminal input test - linux@mirocat

--- RandomProblemSolving/Program.cs
+++ RandomProblemSolving/Program.cs
@@ -6,7 +6,12 @@ namespace RandomProblemSolving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What do you want?");
+            string want = Console.ReadLine();
+
+            Console.WriteLine("Welcome {0} and You want {1}", name, want);
         }
     }
 }
